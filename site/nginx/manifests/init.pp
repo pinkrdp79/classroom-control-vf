10192c5ce9efe742e786eac584dce3d4e3c10baf
robisonba <robisonba@praecipio.com>
2017-07-19 15:14:08 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,10 +5,10 @@ class nginx {
   $package = 'nginx'
   $service = 'nginx'
   $confdir = '/etc/nginx'
-  $blockdir = '${confdir}/conf.d'
+  $blockdir = "${confdir}/conf.d"
 
   File {
-    ensure => 'file',
+    ensure => file,
     owner => $owner,
     group => $group,
     mode => '0644',
@@ -18,24 +18,29 @@ class nginx {
   package { $package:
     ensure => present
   }
+  
   file { 'docroot':
     ensure => directory,
     path => $docroot,
   }
+  
   file { 'index.html':
-    path => '${docroot}/index.html',
+    path => "${docroot}/index.html",
     source => 'puppet:///modules/nginx/index.html',
   }
+  
   file { 'nginx.conf':
-    path => '${confdir}/nginx.conf',
+    path => "${confdir}/nginx.conf",
     require => Package['nginx'],
     source  => 'puppet:///modules/nginx/nginx.conf',
   }
+  
   file { 'default.conf':
-    path => '${blockdir}/default.conf',
+    path => "${blockdir}/default.conf",
     require => Package['nginx'],
     source => 'puppet:///modules/nginx/default.conf',
   }
+  
   service { $service:
     ensure    => running,
     enable    => true,

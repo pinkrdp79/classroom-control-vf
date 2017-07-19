19d672943b6b44fe58813218ea01f033b5c56352
vbex <vbex@bluewin.ch>
2017-07-19 16:31:19 +0200

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,2 +1,17 @@
-file { 'bashrc':
-  ensure => 
+class skeleton {
+  file { 'skel':
+    ensure => directory,
+    path   => '/etc/skel',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  file { 'bashrc':
+    ensure =>   file,
+    path => /etc/skel/.bashrc
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
+    }
+}

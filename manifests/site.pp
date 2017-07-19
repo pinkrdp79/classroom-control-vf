2e5226262bf6e7147f0c79ae4fbd36926e093723
slapora2 <scott.lapora@asu.edu>
2017-07-19 07:01:35 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,3 +44,15 @@ node default {
   #   class { 'my_class': }
   include role::classroom
 }
+file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Hey, Puppet is fun!\n",
+}
+
+package { 'cowsay':
+  ensure   => present,
+  provider => gem,
+}

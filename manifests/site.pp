82a084a34036707fb8d624c77d09bfff401857f9
algallegos <alfonso.gallegos@bluehealthintelligence.com>
2017-07-19 14:37:46 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -54,3 +54,13 @@ node default {
   #   class { 'my_class': }
   include role::classroom
 }
+
+node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
+}
+}

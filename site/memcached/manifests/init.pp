dd603a13a4f5f171aa13712ce93e15c61765c162
martinf999 <wolle07@hotmail.com>
2017-07-19 13:49:11 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,20 @@
+class memcached {
+  package {
+    ensure => present,
+  }
+  
+  file { 'memcached.conf':
+    ensure => file,
+    path   => '/etc/sysconfig/memcached',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet://modules/memcache/mecached.conf',
+  {
+
+  service { 'memcached':
+    ensure    => running,
+    enable    => true,
+    subscribe => [ File['memcached.conf']],
+  }
+}

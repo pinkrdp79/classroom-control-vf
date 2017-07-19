e8bf6f7108939b13381c148a9a210b7185a2617a
martinf999 <wolle07@hotmail.com>
2017-07-19 13:43:16 -0400

    Update init.pp

--- site/memcache/manifests/init.pp
+++ site/memcache/manifests/init.pp
@@ -6,6 +6,9 @@ class memcached {
   file { 'memcached.conf':
     ensure => file,
     path   => '/etc/sysconfig/memcached',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
     source => 'puppet://modules/memcache/mecached.conf',
   {
 

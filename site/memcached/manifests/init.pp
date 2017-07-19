cd4e4b7d31b0ca286f35e6284d99d38418062e57
cwsolie <chucksolie@yahoo.com>
2017-07-19 13:39:04 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,19 @@
+class memcached {
+  package { 'memcached':
+    ensure => present,
+    }
+  file { 'memcached':
+    path => '/etc/sysconfig/',
+    ensure => file,
+    owner => root,
+    group => root,
+    mode => '0644',
+    source => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+    }
+  service { 'memcached':
+    ensure => running,
+    enabled => true,
+    subscribe => File['memcached'],
+    }
+}

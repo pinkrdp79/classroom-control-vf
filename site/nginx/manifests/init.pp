d1acd0539085d41dd9db3e21211cf9b1beb9e007
Himanshu dwivedi <himanshu.dwivedi@safeway.com>
2017-07-19 12:01:04 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,11 +1,11 @@
 class nginx {
   package { 'nginx':
     ensure => present,
-    before => [                                                  # Break arrays up into multiple
-      File['index.html'],                                        # lines as to not go beyond the
-      File['nginx.conf'],                                        # 80 characters per line and it
-      File['default.conf'],                                      # makes it easier to add new
-    ]                                                            # elements and easier to read.
+    before => [                                                  
+      File['index.html'],                                        
+      File['nginx.conf'],                                        
+      File['default.conf'],                                      
+    ]                                                            
   }
 
   file { 'docroot':
@@ -16,27 +16,27 @@ class nginx {
     mode   => '0755',
   }
 
-  file { 'index.html':                                           # use titles instead of full paths
+  file { 'index.html':                                           
     ensure => file,
-    path   => '/var/www/index.html',                             # using namevars make it easy to reference them later
+    path   => '/var/www/index.html',                             
     owner  => 'root',
     group  => 'root',
     mode   => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
 
-  file { 'nginx.conf':                                           # use title
+  file { 'nginx.conf':                                           
     ensure => file,
-    path   => '/etc/nginx/nginx.conf',                           # use of namevar
+    path   => '/etc/nginx/nginx.conf',                           
     owner  => 'root',
     group  => 'root',
     mode   => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
 
-  file { 'default.conf':                                         # use of title
+  file { 'default.conf':                                         
     ensure => file,
-    path   => '/etc/nginx/conf.d/default.conf',                  # use of namevar
+    path   => '/etc/nginx/conf.d/default.conf',                  
     owner  => 'root',
     group  => 'root',
     mode   => '0664',
@@ -46,6 +46,6 @@ class nginx {
   service { 'nginx':
     ensure    => running,
     enable    => true,
-    subscribe => [ File['nginx.conf'], File['default.conf'] ],   # use of single line array, small 1 or 2 elements only
+    subscribe => [ File['nginx.conf'], File['default.conf'] ],   
   }
 }

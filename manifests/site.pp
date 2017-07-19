45995c779a3ba26dfca8a4700a48f609d66f8951
marykelsch <maryqi@gmail.com>
2017-07-19 09:29:57 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,14 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  include examples::fundamentals
+  
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "Hey, Puppet is fun!\n",
+  }
+
 }

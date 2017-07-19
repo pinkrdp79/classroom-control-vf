d65daa78fedf02ffa0bb3a83015daa115b47a3b2
marykelsch <maryqi@gmail.com>
2017-07-19 13:43:48 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -8,7 +8,7 @@ class memcached {
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
-      source  => 'puppet:///modules/memcahed/memcached',
+      source  => 'puppet:///modules/memcached/memcached',
       require => Package['memcached'],
     }
     

982f44baa6ef5f31520c1ddcb76bd00dadf5d297
marykelsch <maryqi@gmail.com>
2017-07-19 09:53:18 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,13 +45,18 @@ node default {
   include role::classroom
   #include examples::fundamentals
   
-  file { 'motd':
-    ensure  => file,
-    path    => '/etc/motd',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "Isn't Puppet fun!\n",
-  }
+ # file { 'motd':
+ #   ensure  => file,
+ #   path    => '/etc/motd',
+ #   owner   => 'root',
+ #   group   => 'root',
+ #   mode    => '0644',
+ #   content => "Isn't Puppet fun!\n",
+ # }
 
+ exec { 'motd':
+   command  => "cowsay 'Welcome to $fqdm!' > /etc/motd",
+   path     => '/user/local/bin',
+   creates  => '/etc/motd',
+ }
 }

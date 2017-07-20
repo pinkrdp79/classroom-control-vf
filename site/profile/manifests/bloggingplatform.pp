e5c308e965f2f02476d8e5e86f0eb9b95c3f9dbb
robisonba <robisonba@praecipio.com>
2017-07-20 14:59:29 -0400

    Create bloggingplatform.pp

---
+++ site/profile/manifests/bloggingplatform.pp
@@ -0,0 +1,11 @@
+class profile::bloggingplatform {
+  include apache
+  include ::mysql::server
+  include wordpress
+  
+  class { '::mysql::server':
+    root_password           => 'strongpassword',
+    remove_default_accounts => true,
+    override_options        => $override_options
+  }
+}

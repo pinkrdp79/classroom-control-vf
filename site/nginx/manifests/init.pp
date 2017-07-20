a8973bdb1f5c1015c969d9953eceebf94494131f
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-07-20 11:45:11 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -10,55 +10,7 @@ class nginx (
   String  $logdir   = $nginx::params::logdir,
   String  $user     = $nginx::params::user,
 ) inherits nginx::params {  
-  package { $package:
-    ensure => present,
-    before => [                                                  
-      File['index.html'],                                        
-      File['nginx.conf'],                                        
-      File['default.conf'],                                      
-    ]                                                            
-  }
-
-  File {
-    ensure => file,
-    owner  => $owner,
-    group  => $group,
-    mode   => '0664',
-  }
-  
-  file { 'docroot':
-    ensure => directory,
-    path   => $docroot,
-  }
-
-  file { 'index.html':                                          
-    path    => "${docroot}/index.html",                            
-    content => epp('nginx/index.html.epp'),
-  }
-
-  file { 'nginx.conf':                                          
-    path    => "${confdir}/nginx.conf",
-    content => epp('nginx/nginx.conf.epp',
-      {
-        user     => $user,
-        confdir  => $confdir,
-        blockdir => $blockdir,
-        logdir   => $logdir,
-      })
-  }
-
-  file { 'default.conf':
-    path    => "${blockdir}/default.conf",
-    content => epp('nginx/default.conf.epp',
-      {
-        docroot => $docroot,
-        port    => $port,
-      }),
-  }
-
-  service { $service:
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['nginx.conf'], File['default.conf'] ],  
-  }
+  include nginx::packages
+  include nginx::config
+  include nginx::services
 }

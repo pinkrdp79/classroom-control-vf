5a2cda605581d4ef575df02a8112408c36f0a8f7
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-07-20 11:49:54 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,14 +1,14 @@
 class nginx (  
-  String  $docroot  = $nginx::params::docroot,
-  String  $owner    = $nginx::params::owner,
-  String  $group    = $nginx::params::group,
-  String  $package  = $nginx::params::package,
-  String  $service  = $nginx::params::service,
-  Integer $port     = $nginx::params::port,
-  String  $confdir  = $nginx::params::confdir,
-  String  $blockdir = $nginx::params::blockdir,
-  String  $logdir   = $nginx::params::logdir,
-  String  $user     = $nginx::params::user,
+  String $docroot  = $nginx::params::docroot,
+  String $owner    = $nginx::params::owner,
+  String $group    = $nginx::params::group,
+  String $package  = $nginx::params::package,
+  String $service  = $nginx::params::service,
+  String $port     = $nginx::params::port,
+  String $confdir  = $nginx::params::confdir,
+  String $blockdir = $nginx::params::blockdir,
+  String $logdir   = $nginx::params::logdir,
+  String $user     = $nginx::params::user,
 ) inherits nginx::params {  
   include nginx::packages
   include nginx::config

1839ccfc8ea2d3fb3429c8ed6559b16da0796d2c
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-07-20 09:13:29 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,12 +11,12 @@ ini_setting { 'random ordering':
   value   => 'title-hash',
 }
 
-node josephoaks.puppetlabs.vm {
-  if $facts['virtual'] == 'docker' {
-    $vmname = $facts['virtual']
-    notify { "This is a ${vmname} machine.": }
-  }
-}
+#node josephoaks.puppetlabs.vm {
+#  if $facts['virtual'] == 'docker' {
+#    $vmname = $facts['virtual']
+#    notify { "This is a ${vmname} machine.": }
+#  }
+#}
 
 node default {
   include role::classroom

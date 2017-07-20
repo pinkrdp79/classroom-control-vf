b32453fa983f20f7a5a6e36924da69538d987b5d
robisonba <robisonba@praecipio.com>
2017-07-20 12:01:52 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,29 +1,33 @@
 class nginx::params {
-  class nginx {
-    case $facts['os']['family'] {
-      'redhat','debian': {
-        $package = 'nginx'
-        $owner = 'root'
-        $group = 'root'
-        $confdir = '/etc/nginx'
-        $docroot = '/var/www'
-        $blockdir = "${confdir}/conf.d"
-        $logdir = '/var/log/nginx'
-        $service = 'nginx'
-      }
-      'windows': {
-        $package = 'nginx-service'
-        $owner = 'Administrator'
-        $group = 'Administrators'
-        $confdir = 'C:/ProgramData/nginx'
-        $docroot = "${confdir}/html"
-        $blockdir = "${confdir}/conf.d"
-        $logdir = "${confdir}/logs"
-        $service = 'nginx'
-      }
-      default :{
-        fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-      }
+  case $facts['os']['family'] {
+    'redhat','debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $confdir = '/etc/nginx'
+      $docroot = '/var/www'
+      $blockdir = "${confdir}/conf.d"
+      $logdir = '/var/log/nginx'
+      $service = 'nginx'
     }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $confdir = 'C:/ProgramData/nginx'
+      $docroot = "${confdir}/html"
+      $blockdir = "${confdir}/conf.d"
+      $logdir = "${confdir}/logs"
+      $service = 'nginx'
+    }
+    default :{
+      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+    }
+  }
+  
+  $runas = $facts['os']['family'] ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
   }
 }

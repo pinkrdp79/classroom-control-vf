18e2bd7a9e076ae6d84f821b053fed2029242bdc
Jeff <jeffr@doit.tamu.edu>
2017-07-20 08:05:12 -0500

    Update init.pp

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -1,8 +1,16 @@
-user { 'admin':
-  ensure => present,
-}
-
-class { 'aliases':
-  admin   => 'admin',
-  require => User['admin'],
+class aliases (
+    $admin = 'root',
+) {
+    # uses $admin to build the aliases file
+    file { '/etc/aliases':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => epp('aliases/aliases.epp', { admin => $admin }),
+    }
+    exec { '/usr/bin/newaliases':
+        refreshonly => true,
+        subscribe   => File['/etc/aliases'],
+    }
 }

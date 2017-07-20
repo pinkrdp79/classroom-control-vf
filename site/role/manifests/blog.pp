4551e7948fc7575c6b1bf38356a3c898064414d3
Tim Nee <timnnee@gmail.com>
2017-07-20 15:36:58 -0400

    Update blog.pp

--- site/role/manifests/blog.pp
+++ site/role/manifests/blog.pp
@@ -2,7 +2,6 @@ class role::blog {
   #web server / php
   include profile::apache
 
-  #
-  # include profile::wordpress
-  # include profile::mysql
+  include profile::wordpress
+  include profile::mysql
 }

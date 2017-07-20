f7ceb8394669d6cf2b38382ade6cae440beb2a24
Jeff <jeffr@doit.tamu.edu>
2017-07-20 08:11:50 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,13 +1,38 @@
 class nginx {
-  $docroot = '/var/www'
-  $owner   = 'root'
-  $group   = 'root'
-  $package = 'nginx'
-  $service = 'nginx'
-  $confdir = '/etc/nginx'
-  $blockdir = "${confdir}/conf.d"
+  case $facts['os']['family'] {
+    'redhat' : {  
+      $docroot  = '/var/www'
+      $owner    = 'root'
+      $group    = 'root'
+      $package  = 'nginx'
+      $service  = 'nginx'
+      $port     = '80'
+      $confdir  = '/etc/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = '/var/log/nginx'
+    }
+    'windows' : {
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $package  = 'nginx'
+      $service  = 'nginx'
+      $port     = '80'
+      $confdir  = 'C:/ProgramData/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $confdir  = 'C:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail("${module_name} is not support on ${facts['os']['family']}")
+    }
+  }
   
- package { $package:
+  $user = $facts['os']['family'] ? {
+    'windows' => 'nobody',
+    default  => 'nginx',
+  }
+  
+  package { $package:
     ensure => present,
     before => [                                                  
       File['index.html'],                                        
@@ -15,37 +40,50 @@ class nginx {
       File['default.conf'],                                      
     ]                                                            
   }
-   
+
   File {
     ensure => file,
-    owner => $owner,
-    group => $group,
-    mode  => '0664',
+    owner  => $owner,
+    group  => $group,
+    mode   => '0664',
   }
   
-  file {'docroot':
+  file { 'docroot':
     ensure => directory,
-    path => $docroot,
+    path   => $docroot,
   }
 
-  file { 'index.html':                                          # use titles instead of full paths
-    path => "${docroot}/index.html",
-    source => 'puppet:///modules/nginx/index.html',
+  file { 'index.html':                                          
+    path    => "${docroot}/index.html",                            
+    #source => 'puppet:///modules/nginx/index.html',
+    content => epp('nginx/index.html.epp'),
   }
 
-  file { 'nginx.conf':                                           # use title
-    path   => "${confdir}/nginx.conf",                            # use of namevar
-    source => 'puppet:///modules/nginx/nginx.conf',
+  file { 'nginx.conf':                                          
+    path    => "${confdir}/nginx.conf",
+    #source => 'puppet:///modules/nginx/nginx.conf',
+    content => epp('nginx/nginx.conf.epp',
+      {
+        user     => $user,
+        confdir  => $confdir,
+        blockdir => $blockdir,
+        logdir   => $logdir,
+      })
   }
 
-  file { 'default.conf':                                         # use of title
-    path   => "${blockdir}/default.conf",                  # use of namevar
-    source => 'puppet:///modules/nginx/default.conf',
-    }
+  file { 'default.conf':
+    path    => "${blockdir}/default.conf",
+    #source => 'puppet:///modules/nginx/default.conf',
+    content => epp('nginx/default.conf.epp',
+      {
+        docroot => $docroot,
+        port    => $port,
+      }),
+  }
 
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => [ File['nginx.conf'], File['default.conf'] ],   # use of single line array, small 1 or 2 elements only
+  service { $service:
+    ensure    => running,
+    enable    => true,
+    subscribe => [ File['nginx.conf'], File['default.conf'] ],  
   }
 }

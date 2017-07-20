f90344ce50986d02afcd942c6e522b11131fece0
Himanshu dwivedi <himanshu.dwivedi@safeway.com>
2017-07-20 10:10:59 -0700

    Create params.pp

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,34 @@
+class nginx::params {
+  case $facts['os']['family'] {
+    'redhat' : {  
+      $docroot  = '/var/www'
+      $owner    = 'root'
+      $group    = 'root'
+      $package  = 'nginx'
+      $service  = 'nginx'
+      $port     = '80'
+      $confdir  = '/etc/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = '/var/log/nginx'
+    }
+    'windows' : {
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $package  = 'nginx'
+      $service  = 'nginx'
+      $port     = '80'
+      $confdir  = 'C:/ProgramData/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $confdir  = 'C:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail("${module_name} is not support on ${facts['os']['family']}")
+    }
+  }
+  
+  $user = $facts['os']['family'] ? {
+    'windows' => 'nobody',
+    default  => 'nginx',
+  }
+}

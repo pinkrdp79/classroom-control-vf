8f9b4a75bdfdef7fdb2eed500728f7ed71232a2d
robisonba <robisonba@praecipio.com>
2017-07-20 12:03:45 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,13 +1,13 @@
 class nginx (
-    String $package = $nginx::params::package
-    String $owner = $nginx::params::owner
-    String $group = $nginx::params::group
-    String $confdir = $nginx::params::confdir
-    String $docroot = $nginx::params::docroot
-    String $blockdir = $nginx::params::blockdir
-    String $logdir = $nginx::params::logdir
-    String $service = $nginx::params::service
-    String $runas = $nginx::params::runas
+    String $package = $nginx::params::package,
+    String $owner = $nginx::params::owner,
+    String $group = $nginx::params::group,
+    String $confdir = $nginx::params::confdir,
+    String $docroot = $nginx::params::docroot,
+    String $blockdir = $nginx::params::blockdir,
+    String $logdir = $nginx::params::logdir,
+    String $service = $nginx::params::service,
+    String $runas = $nginx::params::runas,
 ) inherits nginx::params {
 
   File {

246d727224d9b871c51695854f13bbb5ab8f964e
robisonba <robisonba@praecipio.com>
2017-07-20 11:59:00 -0400

    Create params.pp

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,29 @@
+class nginx::params {
+  class nginx {
+    case $facts['os']['family'] {
+      'redhat','debian': {
+        $package = 'nginx'
+        $owner = 'root'
+        $group = 'root'
+        $confdir = '/etc/nginx'
+        $docroot = '/var/www'
+        $blockdir = "${confdir}/conf.d"
+        $logdir = '/var/log/nginx'
+        $service = 'nginx'
+      }
+      'windows': {
+        $package = 'nginx-service'
+        $owner = 'Administrator'
+        $group = 'Administrators'
+        $confdir = 'C:/ProgramData/nginx'
+        $docroot = "${confdir}/html"
+        $blockdir = "${confdir}/conf.d"
+        $logdir = "${confdir}/logs"
+        $service = 'nginx'
+      }
+      default :{
+        fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+      }
+    }
+  }
+}

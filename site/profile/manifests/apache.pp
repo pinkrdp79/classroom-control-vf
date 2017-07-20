498c3c65b66ca36ba7f289258d784bfa1f1a4ca8
martinf999 <wolle07@hotmail.com>
2017-07-20 15:19:29 -0400

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,5 +1,5 @@
 class profile::wordpress { 'wordpress' :
     ensure => present,
-    notify { 'This is the example profile!': } 
+    notify { 'This is the example profile!': },
     }
 class profile::apache { 'apache' : }

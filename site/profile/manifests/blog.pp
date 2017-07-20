5d754ebfba298e11efc9604148bdd17f966d8927
marykelsch <maryqi@gmail.com>
2017-07-20 15:43:16 -0400

    Update blog.pp

--- site/profile/manifests/blog.pp
+++ site/profile/manifests/blog.pp
@@ -1,1 +1,25 @@
+class prefile::blog {
+  #web server / php
+  class {'apache:
+    docroot  => '/var/www',
+    #port     => '8080',
+  }
+  
+  class {'apche::mod::php':}
+  
+  #db server
+  class {'mysql::server':
+    root_password  => 'somepassword',
+  }
+  
+  class {'mysql::bindings':
+    php_enable  => true,
+    #java_enable => true,
+  }
 
+  #blogging platform (wordpress)
+  class {'wordpress':
+    install_dir  => '/var/www/wp',
+  }
+
+}

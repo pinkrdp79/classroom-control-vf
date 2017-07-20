18129c3f7fcf7b532fdb14267f9cf09524fad578
Himanshu dwivedi <himanshu.dwivedi@safeway.com>
2017-07-20 11:54:28 -0700

    Create packages.pp

---
+++ site/nginx/manifests/packages.pp
@@ -0,0 +1,10 @@
+class nginx::packages inherits nginx::params {
+  package { $package:
+    ensure => present,
+    before => [                                                  
+      File['index.html'],                                        
+      File['nginx.conf'],                                        
+      File['default.conf'],                                      
+    ]                                                            
+  }
+}

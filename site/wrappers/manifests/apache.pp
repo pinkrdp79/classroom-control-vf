fb2afbf95f7e3a615e9b7780cfa38af0ab3ea7c6
Jeff <jeffr@doit.tamu.edu>
2017-07-20 14:09:39 -0500

    Update apache.pp

--- site/wrappers/manifests/apache.pp
+++ site/wrappers/manifests/apache.pp
@@ -1,1 +1,10 @@
+class { 'apache': }
+  default_vhost => false,
 
+apache::vhost { 'user.example.com':
+  port          => '80',
+  docroot       => '/var/www/user',
+  docroot_owner => 'www-data',
+  docroot_group => 'www-data',
+}
+  

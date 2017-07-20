b866aa1f1171cfd5be84eaa595a0de72139a40a1
slapora2 <scott.lapora@asu.edu>
2017-07-20 08:10:16 -0700

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,3 +1,9 @@
 class profile::base {
   notify { "Hello, my name is ${::hostname}": }
 }
+
+class profile::base {
+# This is where you can declare classes for all nodes. # Example:
+# class { 'my_class': }
+$message = hiera('message')
+  notify { $message: } }

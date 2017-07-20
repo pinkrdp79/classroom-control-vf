54f26ab4cde498699e64c5260eb176e127ff7d5a
Ronnie Pinkerton <pinkrdp@gmail.com>
2017-07-20 09:02:03 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,13 +1,26 @@
 define users::managed_user (
   $group = $title,
-  ) {
-    user { $title:
-    ensure => present,
+){
+  $home = $facts['os']['family'] ? {
+    'windows' => 'C:/Users',
+    default   => '/home',
+  }
+  
+  user { $title:
+    ensure     => present,
+    managehome => true,
   }
 
-  file { "/home/${title}":
+  file { "${home}/${title}":
     ensure => directory,
-    owner => $title,
-    group => $group,
+    owner  => $title,
+    group  => $group,
+  }
+  
+  file { "${home}/${title}/.ssh":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+    mode   => '0700',
   }
 }

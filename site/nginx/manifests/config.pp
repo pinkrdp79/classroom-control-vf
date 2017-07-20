0b382c2343f171fd69ca1273ebf087c8eeed951e
marykelsch <maryqi@gmail.com>
2017-07-20 13:11:03 -0400

    Create config.pp

---
+++ site/nginx/manifests/config.pp
@@ -0,0 +1,38 @@
+class nginx::config {
+  File {
+    ensure => file,
+    owner  => $owner,
+    group  => $group,
+    mode   => '0664',
+  }
+  
+  file { 'docroot':
+    ensure => directory,
+    path   => $docroot,
+  }
+
+  file { 'index.html':                                          
+    path    => "${docroot}/index.html",                            
+    content => epp('nginx/index.html.epp'),
+  }
+
+  file { 'nginx.conf':                                          
+    path    => "${confdir}/nginx.conf",
+    content => epp('nginx/nginx.conf.epp',
+      {
+        user     => $user,
+        confdir  => $confdir,
+        blockdir => $blockdir,
+        logdir   => $logdir,
+      })
+  }
+
+  file { 'default.conf':
+    path    => "${blockdir}/default.conf",
+    content => epp('nginx/default.conf.epp',
+      {
+        docroot => $docroot,
+        port    => $port,
+      }),
+  }
+}

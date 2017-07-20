aaee904d15ce40570ef7de7210042dfb17c6497c
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-07-20 07:56:13 -0600

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,6 +1,12 @@
 class users::admins {
   $user = [ 'alice', 'chen', 'jose' ]
 
-
-
+  users::managed_user { $user:
+    ensure => present,
+    group  => 'staff',
+  }
+  
+  group { 'staff':
+    ensure => present,
+  }
 }

c3c4c76705458946f38fe68f36d26f98d5c191c1
vbex <vbex@bluewin.ch>
2017-07-20 21:35:52 +0200

    Update blog.pp

--- site/profile/manifests/blog.pp
+++ site/profile/manifests/blog.pp
@@ -1,9 +1,7 @@
-class profile::blog{
-  class {'apache':
-  }
-  class  {'apache::mod:php'
-    docroot => '/var/www'
-  }
-  include 'mysql::server'
-  include wordpress
+class profile::blog {
+  class { 'apache': docroot => '/var/www', }
+  class { 'apache::mod::php': }
+  class { 'mysql::server': root_password => 'supersecret', }
+  class { 'mysql::bindings': php_enable => true, }
+  class { 'wordpress': install_dir => '/var/www/wp', }
 }

0b9856e1fd3e74f42294d9cd7ca35212f27d751e
slapora2 <scott.lapora@asu.edu>
2017-07-20 12:40:53 -0700

    Create blog.pp

---
+++ site/profile/manifests/blog.pp
@@ -0,0 +1,8 @@
+class profile::blog {
+  class { 'apache':
+    docroot => '/var/www',
+  }
+  class { 'apache::mod::php': }
+  class { 'mysql::server': root_password => 'supersecret', }
+  class { 'mysql::bindings': php_enable => true, }
+  class { 'wordpress': install_dir => '/var/www/wp', }

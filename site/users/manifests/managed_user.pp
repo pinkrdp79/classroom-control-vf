e2ef88579834116eb3166c1a83069eead60c1ed9
Jeff <jeffr@doit.tamu.edu>
2017-07-20 08:53:59 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,12 +1,26 @@
 define users::managed_user (
- $group = $title,
-) {
-user { $title:
- ensure => present,
-}
-file { "/home/${title}":
- ensure => directory,
- owner => $title,
- group => $group,
- }
+  $group = $title,
+){
+  $home = $facts['os']['family'] ? {
+    'windows' => 'C:/Users',
+    default   => '/home',
+  }
+  
+  user { $title:
+    ensure     => present,
+    managehome => true,
+  }
+
+  file { "${home}/${title}":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+  }
+  
+  file { "${home}/${title}/.ssh":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+    mode   => '0700',
+  }
 }

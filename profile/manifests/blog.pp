9631cd25121d3c17283a2e78060216d7983349b1
marykelsch <maryqi@gmail.com>
2017-07-20 15:32:32 -0400

    Create blog.pp

---
+++ profile/manifests/blog.pp
@@ -0,0 +1,25 @@
+class prefile::blog {
+
+  #web server / php
+  class {'apache:
+    docroot  => '/var/www',
+  }
+  
+  class {'apche::mod::php':}
+  
+  #db server
+  class {'mysql::server':
+    root_password  => 'somepassword',
+  }
+  
+  class {'mysql::bindings':
+    php_enable  => true,
+    #java_enable => true,
+  }
+
+  #blogging platform (wordpress)
+  class {'wordpress':
+    install_dir  => '/var/www/wp',
+  }
+
+}

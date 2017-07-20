dee3df683a215b0d1a582702f93fdf75b50dad67
Ronnie Pinkerton <pinkrdp@gmail.com>
2017-07-20 14:19:09 -0500

    Update init.pp

--- site/blogsite/manifests/init.pp
+++ site/blogsite/manifests/init.pp
@@ -1,12 +1,13 @@
 class blogsite {
+  include '::mysql::server'
   user { 'wordpress':
     ensure     => present,
     managehome => true,
   }
   class { 'wordpress':
-  wp_owner    => 'wordpress',
-  wp_group    => 'wordpress',
-  db_user     => 'wordpress',
-  db_password => 'puppetlabs',
-}
+    wp_owner    => 'wordpress',
+    wp_group    => 'wordpress',
+    db_user     => 'wordpress',
+    db_password => 'puppetlabs',
+  }
 }

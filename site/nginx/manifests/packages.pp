b262577b8f4de4b821fa0c71824404c511ac87e9
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-07-20 10:54:35 -0600

    Update packages.pp

--- site/nginx/manifests/packages.pp
+++ site/nginx/manifests/packages.pp
@@ -1,3 +1,10 @@
 class nginx::packages {
-
+  package { $package:
+    ensure => present,
+    before => [                                                  
+      File['index.html'],                                        
+      File['nginx.conf'],                                        
+      File['default.conf'],                                      
+    ]                                                            
+  }
 }

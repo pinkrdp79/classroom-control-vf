83971ff6d3356fe2434977cd136532beb969c93e
Jeff <jeffr@doit.tamu.edu>
2017-07-20 14:41:11 -0500

    Update blog.pp

--- site/profile/manifests/blog.pp
+++ site/profile/manifests/blog.pp
@@ -1,24 +1,7 @@
 class profile::blog {
-#web server /php
-  class{'apache':
-    docroot => '/var/www',
-  }  
-
-class {'apache::mod::php':}
-
-#DB Server
-class {'mysql::server'}
-  root_password => 'supersecret'
-  }
-
-class {'mysql::bindings::php'}
-
-class {'mysql::bindings':
-  php_enable => true,
-  }
-
-#blogging platform (wordpress)
-  class {'wordpress':
-    install_dir => '/var/www/wp',
-  }
+  class { 'apache': docroot => '/var/www', }
+  class { 'apache::mod::php': }
+  class { 'mysql::server': root_password => 'supersecret', }
+  class { 'mysql::bindings': php_enable => true, }
+  class { 'wordpress': install_dir => '/var/www/wp', }
 }

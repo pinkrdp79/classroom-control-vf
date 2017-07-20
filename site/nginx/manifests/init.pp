fab4791e0376b42332a03c7e085f82b465704577
Himanshu dwivedi <himanshu.dwivedi@safeway.com>
2017-07-20 06:53:37 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,12 +1,37 @@
 
 class nginx {
-  $docroot  = '/var/www'
-  $owner    = 'root'
-  $group    = 'root'
-  $package  = 'nginx'
-  $service  = 'nginx'
-  $confdir  = '/etc/nginx'
-  $blockdir = "${confdir}/conf.d"
+  case $facts['os']['family'] {
+    'redhat' : {  
+      $docroot  = '/var/www'
+      $owner    = 'root'
+      $group    = 'root'
+      $package  = 'nginx'
+      $service  = 'nginx'
+      $port     = '80'
+      $confdir  = '/etc/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = '/var/log/nginx'
+    }
+    'windows' : {
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $package  = 'nginx'
+      $service  = 'nginx'
+      $port     = '80'
+      $confdir  = 'C:/ProgramData/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $confdir  = 'C:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail("${module_name} is not support on ${facts['os']['family']}")
+    }
+  }
+  
+  $user = $facts['os']['family'] ? {
+    'windows' => 'nobody',
+    default  => 'nginx',
+  }
   
   package { $package:
     ensure => present,
@@ -30,18 +55,31 @@ class nginx {
   }
 
   file { 'index.html':                                          
-    path   => "${docroot}/index.html",                            
-    source => 'puppet:///modules/nginx/index.html',
+    path    => "${docroot}/index.html",                            
+    #source => 'puppet:///modules/nginx/index.html',
+    content => epp('nginx/index.html.epp'),
   }
 
   file { 'nginx.conf':                                          
-    path   => "${confdir}/nginx.conf",                          
-    source => 'puppet:///modules/nginx/nginx.conf',
+    path    => "${confdir}/nginx.conf",
+    #source => 'puppet:///modules/nginx/nginx.conf',
+    content => epp('nginx/nginx.conf.epp',
+      {
+        user     => $user,
+        confdir  => $confdir,
+        blockdir => $blockdir,
+        logdir   => $logdir,
+      })
   }
 
-  file { 'default.conf':                                        
-    path   => "${blockdir}/default.conf",                 
-    source => 'puppet:///modules/nginx/default.conf',
+  file { 'default.conf':
+    path    => "${blockdir}/default.conf",
+    #source => 'puppet:///modules/nginx/default.conf',
+    content => epp('nginx/default.conf.epp',
+      {
+        docroot => $docroot,
+        port    => $port,
+      }),
   }
 
   service { $service:
